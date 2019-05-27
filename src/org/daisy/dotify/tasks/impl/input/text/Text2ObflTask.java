8e41613773519fccd46928636122b71c89367bf8
Joel Håkansson <joel.hakansson@pm.me>
2019-05-27 15:26:24 +0200

    Add options to Text2ObflTask

--- src/org/daisy/dotify/tasks/impl/input/text/Text2ObflTask.java
+++ src/org/daisy/dotify/tasks/impl/input/text/Text2ObflTask.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,14 +15,22 @@ import org.daisy.dotify.common.xml.XMLTools;
 import org.daisy.streamline.api.media.AnnotatedFile;
 import org.daisy.streamline.api.media.DefaultAnnotatedFile;
 import org.daisy.streamline.api.option.UserOption;
+import org.daisy.streamline.api.option.UserOptionValue;
 import org.daisy.streamline.api.tasks.InternalTaskException;
 import org.daisy.streamline.api.tasks.ReadWriteTask;
 
 class Text2ObflTask extends ReadWriteTask {
+	private static final Logger LOGGER = Logger.getLogger(Text2ObflTask.class.getCanonicalName());
 	private static final String SOURCE_ENCODING = "source-encoding";
 	private static final String SOURCE_LANGUAGE = "source-language";
 	private static final String PAGE_WIDTH = "page-width";
 	private static final String PAGE_HEIGHT = "page-height";
+	private static final String DUPLEX = "duplex";
+	private static final String INNER_MARGIN = "inner-margin";
+	private static final String OUTER_MARGIN = "outer-margin";
+	private static final String ROW_SPACING = "row-spacing";
+	private static final String SHOW_BRAILLE_PAGE_NUMBERS = "show-braille-page-numbers";
+	
 	private static final String DEFAULT_ENCODING = "utf-8";
 	private final String encoding;
 	private final String rootLang;
@@ -58,33 +67,47 @@ class Text2ObflTask extends ReadWriteTask {
 					.orElse(DEFAULT_ENCODING));
 			fw.setRootLang(getLanguage());
 		
-			{
-				Object v = params.get(PAGE_WIDTH);
-				if (v!=null) {
-					try {
-						fw.setWidth(Integer.parseInt(v.toString()));
-					} catch (Exception e) {
-						Logger.getLogger(this.getClass().getCanonicalName()).log(Level.WARNING, "Failed to set page width.", e);
-					}
-				}
-			}
-			{
-				Object v = params.get(PAGE_HEIGHT);
-				if (v!=null) {
-					try {
-						fw.setHeight(Integer.parseInt(v.toString()));
-					} catch (Exception e) {
-						Logger.getLogger(this.getClass().getCanonicalName()).log(Level.WARNING, "Failed to set page height.", e);
-					}
-				}
-				fw.parse();
-			}
+			asIntParam(PAGE_WIDTH).ifPresent(v->fw.setWidth(v));
+			asIntParam(PAGE_HEIGHT).ifPresent(v->fw.setHeight(v));
+			asIntParam(INNER_MARGIN).ifPresent(v->fw.setInnerMargin(v));
+			asIntParam(OUTER_MARGIN).ifPresent(v->fw.setOuterMargin(v));
+			asDoubleParam(ROW_SPACING).ifPresent(v->fw.setRowSpacing(v));
+			asBooleanParam(DUPLEX).ifPresent(v->fw.setDuplex(v));
+			asBooleanParam(SHOW_BRAILLE_PAGE_NUMBERS).ifPresent(v->fw.setShowBraillePageNumbers(v));
+			fw.parse();
+
 		} catch (FileNotFoundException e) {
 			throw new InternalTaskException("FileNotFoundException", e);
 		} catch (IOException e) {
 			throw new InternalTaskException("IOException", e);
 		}
 		return new DefaultAnnotatedFile.Builder(output.toPath()).extension("obfl").mediaType("application/x-obfl+xml").build();
+	}
+	
+	private Optional<Integer> asIntParam(String key) {
+		return Optional.ofNullable(params.get(key)).map(v->{
+			try {
+				return Integer.parseInt(v.toString());
+			} catch (Exception e) {
+				LOGGER.log(Level.WARNING, String.format("Failed to set %s.", key), e);
+				return null;
+			}
+		});
+	}
+	
+	private Optional<Double> asDoubleParam(String key) {
+		return Optional.ofNullable(params.get(key)).map(v->{
+			try {
+				return Double.parseDouble(v.toString());
+			} catch (Exception e) {
+				LOGGER.log(Level.WARNING, String.format("Failed to set %s.", key), e);
+				return null;
+			}
+		});
+	}
+	
+	private Optional<Boolean> asBooleanParam(String key) {
+		return Optional.ofNullable(params.get(key)).map(v->Boolean.valueOf(v.toString()));
 	}
 
 	@Override
@@ -97,10 +120,45 @@ class Text2ObflTask extends ReadWriteTask {
 	public List<UserOption> getOptions() {
 		if (options==null) {
 			options = new ArrayList<>();
-			options.add(new UserOption.Builder(PAGE_WIDTH).description("The width of the page").defaultValue("32").build());
-			options.add(new UserOption.Builder(PAGE_HEIGHT).description("The height of the page").defaultValue("29").build());
-			options.add(new UserOption.Builder(SOURCE_ENCODING).description("The encoding of the input file").defaultValue("[detect]").build());
-			options.add(new UserOption.Builder(SOURCE_LANGUAGE).description("The language of the input file").defaultValue(rootLang).build());
+			options.add(new UserOption.Builder(PAGE_WIDTH)
+					.description("The width of the page (a positive integer)")
+					.defaultValue(String.valueOf(Text2ObflWriter.DEFAULT_WIDTH))
+					.build());
+			options.add(new UserOption.Builder(PAGE_HEIGHT)
+					.description("The height of the page (a positive integer)")
+					.defaultValue(String.valueOf(Text2ObflWriter.DEFAULT_HEIGHT))
+					.build());
+			options.add(new UserOption.Builder(INNER_MARGIN)
+					.description("The inner margin (a non-negative integer)")
+					.defaultValue(String.valueOf(Text2ObflWriter.DEFAULT_INNER_MARGIN))
+					.build());
+			options.add(new UserOption.Builder(OUTER_MARGIN)
+					.description("The outer margin (a non-negative integer)")
+					.defaultValue(String.valueOf(Text2ObflWriter.DEFAULT_OUTER_MARGIN))
+					.build());
+			options.add(new UserOption.Builder(ROW_SPACING)
+					.description("The row spacing (a number >= 1)")
+					.defaultValue(String.valueOf(Text2ObflWriter.DEFAULT_ROW_SPACING))
+					.build());
+			options.add(new UserOption.Builder(DUPLEX)
+					.description("Layout on both sides of the sheet")
+					.addValue(new UserOptionValue.Builder("true").build())
+					.addValue(new UserOptionValue.Builder("false").build())
+					.defaultValue(String.valueOf(Text2ObflWriter.DEFAULT_DUPLEX)).build());
+			options.add(new UserOption.Builder(SHOW_BRAILLE_PAGE_NUMBERS)
+					.description("Show braille page numbers in the header")
+					.addValue(new UserOptionValue.Builder("true").build())
+					.addValue(new UserOptionValue.Builder("false").build())
+					.defaultValue(String.valueOf(Text2ObflWriter.DEFAULT_SHOW_BRAILLE_PAGE_NUMBERS))
+					.build());
+			options.add(new UserOption.Builder(SOURCE_ENCODING)
+					.description("The encoding of the input file")
+					.defaultValue("[detect]")
+					.build());
+			options.add(new UserOption.Builder(SOURCE_LANGUAGE)
+					.description("The language of the input file")
+					.defaultValue(rootLang)
+					.build());
 		}
 		return options;
 	}
--- src/org/daisy/dotify/tasks/impl/input/text/Text2ObflWriter.java
+++ src/org/daisy/dotify/tasks/impl/input/text/Text2ObflWriter.java
@@ -9,13 +9,30 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 
 class Text2ObflWriter extends Xml2ObflWriter {
+	static final int DEFAULT_HEIGHT = 29;
+	static final int DEFAULT_WIDTH = 32;
+	static final int DEFAULT_INNER_MARGIN = 2;
+	static final int DEFAULT_OUTER_MARGIN = 2;
+	static final double DEFAULT_ROW_SPACING = 1.0;
+	static final boolean DEFAULT_DUPLEX = true;
+	static final boolean DEFAULT_SHOW_BRAILLE_PAGE_NUMBERS = false;
 	private int height;
 	private int width;
+	private int innerMargin;
+	private int outerMargin;
+	private double rowSpacing;
+	private boolean duplex;
+	private boolean showBraillePageNumbers;
 
 	Text2ObflWriter(InputStream is, OutputStream os, String encoding) {
 		super(is, os, encoding);
-		this.height = 29;
-		this.width = 32;
+		this.height = DEFAULT_HEIGHT;
+		this.width = DEFAULT_WIDTH;
+		this.innerMargin = DEFAULT_INNER_MARGIN;
+		this.outerMargin = DEFAULT_OUTER_MARGIN;
+		this.rowSpacing = DEFAULT_ROW_SPACING;
+		this.duplex = DEFAULT_DUPLEX;
+		this.showBraillePageNumbers = DEFAULT_SHOW_BRAILLE_PAGE_NUMBERS;
 	}
 
 	Text2ObflWriter(File input, File output, String encoding) throws FileNotFoundException {
@@ -41,16 +58,83 @@ class Text2ObflWriter extends Xml2ObflWriter {
 	void setWidth(int value) {
 		this.width = value;
 	}
+	
+	int getInnerMargin() {
+		return innerMargin;
+	}
+
+	void setInnerMargin(int innerMargin) {
+		this.innerMargin = innerMargin;
+	}
+
+	int getOuterMargin() {
+		return outerMargin;
+	}
+
+	void setOuterMargin(int outerMargin) {
+		this.outerMargin = outerMargin;
+	}
+
+	double getRowSpacing() {
+		return rowSpacing;
+	}
+
+	void setRowSpacing(double rowSpacing) {
+		this.rowSpacing = rowSpacing;
+	}
+
+	boolean isDuplex() {
+		return duplex;
+	}
+
+	void setDuplex(boolean duplex) {
+		this.duplex = duplex;
+	}
+	
+	boolean isShowBraillePageNumbers() {
+		return showBraillePageNumbers;
+	}
+
+	void setShowBraillePageNumbers(boolean showBraillePageNumbers) {
+		this.showBraillePageNumbers = showBraillePageNumbers;
+	}
 
 	@Override
 	protected void writePrologue(PrintWriter pw) {
 		pw.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 		pw.println("<obfl version=\"2011-1\" xml:lang=\"" + getRootLang() + "\" xmlns=\"http://www.daisy.org/ns/2011/obfl\">");
-		pw.println("<layout-master name=\"default\" page-height=\""+height+"\" page-width=\""+width+"\">");
-		pw.println("<default-template>");
-		pw.println("<header></header>");
-		pw.println("<footer></footer>");
-		pw.println("</default-template>");
+		pw.println(String.format(
+				"<layout-master name=\"default\" page-height=\"%s\" page-width=\"%s\" inner-margin=\"%s\" outer-margin=\"%s\" row-spacing=\"%s\" duplex=\"%s\">", 
+				height, width, innerMargin, outerMargin, rowSpacing, duplex));
+		
+		if (showBraillePageNumbers) {
+			if (duplex) {
+				pw.println("<template use-when=\"(= (% $page 2) 0)\">");
+				pw.println("	<header>");
+				pw.println("		<field>");
+				pw.println("			<string value=\"  \"/>");
+				pw.println("			<current-page number-format=\"default\"/>");
+				pw.println("		</field>");
+				pw.println("		<field/>");
+				pw.println("	</header>");
+				pw.println("	<footer/>");
+				pw.println("</template>");
+			}
+			pw.println("<default-template>");
+			pw.println("	<header>");
+			pw.println("		<field/>");
+			pw.println("		<field>");
+			pw.println("			<current-page number-format=\"default\"/>");
+			pw.println("		</field>");
+			pw.println("	</header>");
+			pw.println("	<footer/>");
+			pw.println("</default-template>");
+		} else {
+			pw.println("<default-template>");
+			pw.println("<header></header>");
+			pw.println("<footer></footer>");
+			pw.println("</default-template>");
+		}
 		pw.println("</layout-master>");
 		pw.println("<sequence master=\"default\">");
 	}
